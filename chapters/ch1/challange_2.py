dcab20fb5986410e0d15303c4668201a0bfd54cc
Passion94 <95107314+Passion94@users.noreply.github.com>
2023-04-22 16:49:27 +0100

    Challange 2

---
+++ chapters/ch1/challange_2.py
@@ -0,0 +1,7 @@
+
+
+season = input('What your best season of the year')
+adjective= input (f'Do you like it hot, warm, cold or very cold during {season}')
+number = input (f'how many cups of juice do you take during a {adjective} {season} ')
+
+print(f'on a {adjective} {season} day, I drink at least {number} cups of juice')
