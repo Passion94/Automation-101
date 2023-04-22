98f46b919d347ff0f4c9fd81818773c46e24d7ae
Passion94 <95107314+Passion94@users.noreply.github.com>
2023-04-22 16:02:37 +0100

    Challange 1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,10 @@
+# my_name = 'Ogodo onyebuchi'
+# about_me = 'I have a lot of things to talk about myself in the first place'
+# Bio = 'Having a full potentional filled with expectations is the most dangerous decision ever made'
+# first_name = input('Please input your first name: ')
+# last_name = input('Please input your last name: ')
+# print("Your full name is " + last_name + ', ' + first_name)
+
+first_name = input('Enter your first name:')
+last_name = input ('Enter your last name:')
+print(f'Your full name is {first_name} {last_name}')
